121d8d7fb4f2f9a0a929d78469b8fdef33bcf704
Yang Kyowon <kyowonhorang@gmail.com>
2018-12-07 23:57:42 +0900

    Added Thirdparty library
    - Assimp
    - glm
    - glad
    - glfw
    - stb_image
    - imgui

--- Sources/rumia/Entry.cpp
+++ Sources/rumia/Entry.cpp
@@ -1,11 +1,8 @@
 #include "Core/EngineDefines.hpp"
-#include "Math/Vector2.hpp"
 
 #include <iostream>
 
 int main()
 {
-   rumia::Vector2 vec = { 0.3f, 20.f };
-   std::cout << vec.xx << ", " << vec.yy << std::endl;
    return 0;
 }
