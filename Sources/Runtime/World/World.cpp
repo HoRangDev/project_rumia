cf18113d1e36f0c07e2e9ae7d8f8f57f666415a2
Yang Kyowon <kyowonhorang@gmail.com>
2018-12-09 01:53:09 +0900

    Added World class

---
+++ Sources/Runtime/World/World.cpp
@@ -0,0 +1,80 @@
+#include "World/World.hpp"
+#include "World/Actor.hpp"
+
+namespace rumia
+{
+   World::World()
+   {
+   }
+
+   World::~World()
+   {
+      Unload();
+   }
+
+   void World::LoadFromFile(const std::string& filePath)
+   {
+      // @TODO: Implement Json serialize/deserialize
+   }
+
+   Actor* World::CreateActor()
+   {
+      Actor* newActor = new Actor();
+      m_actors.emplace_back(newActor);
+   }
+
+   void World::AddActor(Actor* actor)
+   {
+      if (actor != nullptr)
+      {
+         m_actors.emplace_back(actor);
+
+         Transform* transform = actor->GetComponent<Transform>();
+         std::vector<Transform*> children = transform->GetChildren();
+         for (auto child : children)
+         {
+            Actor* childActor = child->GetActor();
+            AddActor(childActor);
+         }
+      }
+   }
+
+   void World::RemoveActor(Actor* actor)
+   {
+      if (actor != nullptr)
+      {
+         auto itr = std::find(m_actors.begin(), m_actors.end(), actor);
+         m_actors.erase(itr);
+
+         // also remove children
+         Transform* transform = actor->GetComponent<Transform>();
+         std::vector<Transform*> children{ std::move(transform->GetChildren()) };
+         for (auto child : children)
+         {
+            Actor* childActor = child->GetActor();
+            RemoveActor(childActor);
+         }
+
+         SafeDelete(actor);
+      }
+   }
+
+   void World::Unload()
+   {
+      for (auto actor : m_actors)
+      {
+         SafeDelete(actor);
+      }
+
+      m_actors.clear();
+      m_actors.shrink_to_fit();
+   }
+
+   void World::Tick()
+   {
+      for (auto actor : m_actors)
+      {
+         actor->Tick();
+      }
+   }
+}
--- Sources/Runtime/World/World.hpp
+++ Sources/Runtime/World/World.hpp
@@ -1,1 +1,31 @@
 #pragma once
+#include "Core/EngineDefines.hpp"
+
+#include <string>
+#include <vector>
+
+namespace rumia
+{
+   class Actor;
+   class RUMIA World
+   {
+   public:
+      ~World();
+
+      void LoadFromFile(const std::string& filePath);
+
+      Actor* CreateActor();
+      void AddActor(Actor* actor);
+      void RemoveActor(Actor* actor);
+
+      void Unload();
+
+      void Tick();
+
+   private:
+      World();
+
+   private:
+      std::vector<Actor*>  m_actors;
+   };
+}
