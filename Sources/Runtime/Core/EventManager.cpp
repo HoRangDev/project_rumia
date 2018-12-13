728f205ab185cc2956454b0017ee69ff1bb522bc
Yang Kyowon <kyowonhorang@gmail.com>
2018-12-13 16:19:34 +0900

    Fixed Sub,Unsub priority
    #23 fixed

--- Sources/Runtime/Core/EventManager.cpp
+++ Sources/Runtime/Core/EventManager.cpp
@@ -8,52 +8,92 @@ namespace rumia
       return inst;
    }
 
-   void EventManager::Subscribe(uint32 eventType, void* subscriber, Callback&& callback)
+   void EventManager::StageChangeEnqueue(uint32 eventType, ESubscriberState state, void* subscriber, Callback&& callback)
    {
-      m_events[eventType].push_back(std::make_pair(
-         subscriber, 
-         std::forward<Callback>(callback)));
-   }
-
-   void EventManager::UnSubscribeEnqueue(uint32 eventType, void* subscriber)
-   {
-      auto itr = m_events.find(eventType);
-      if (itr != m_events.end())
+      if (state == ESubscriberState::Subscribe)
       {
-         m_unsubQueueMap[eventType].push_back(subscriber);
-      }
-   }
-
-   void EventManager::UnSubscribe(uint32 eventType, std::vector<Subscriber>& subscribers)
-   {
-      auto unsubItr = m_unsubQueueMap.find(eventType);
-      if (unsubItr != m_unsubQueueMap.end())
-      {
-         for (auto unsubscriber : unsubItr->second)
+         auto itr = m_events.find(eventType);
+         if (itr == m_events.end())
          {
-            auto foundSubscriber = std::find_if(subscribers.begin(), subscribers.end(),
-               [&unsubscriber](const Subscriber& foundSubscriber) {
-               return foundSubscriber.first == unsubscriber;
-            });
+            // Initial subscribe!
+            SubscribeImmeidate(eventType, subscriber, std::forward<Callback>(callback));
+            return;
+         }
+      }
 
-            if (foundSubscriber != subscribers.end())
+      m_stateChangeQueueMap[eventType].push_back(std::make_pair(
+         state,
+         std::make_pair(subscriber, callback) // Subscriber info
+      ));
+   }
+
+   void EventManager::ProcessChangedState(uint32 eventType, std::vector<Subscriber>& subscribers)
+   {
+      auto stateItr = m_stateChangeQueueMap.find(eventType);
+      if (stateItr != m_stateChangeQueueMap.end())
+      {
+         std::vector<StateChange>& stateChanges = stateItr->second;
+         for (StateChange& stateChange : stateChanges)
+         {
+            Subscriber& subscriber = stateChange.second;
+            switch (stateChange.first)
             {
-               subscribers.erase(foundSubscriber);
+            case ESubscriberState::Subscribe:
+               Subscribe(subscriber.first, std::move(subscriber.second), subscribers);
+               break;
+
+            case ESubscriberState::UnSubscribe:
+               UnSubscribe(subscriber.first, subscribers);
+               break;
             }
          }
 
-         unsubItr->second.clear();
-         //unsubItr->second.shrink_to_fit();
+         stateChanges.clear();
+         //stateChanges.shrink_to_fit();
       }
    }
 
+   void EventManager::Subscribe(void* subscriber, Callback&& callback, std::vector<Subscriber>& subscribers)
+   {
+      // @TODO: No duplicate subscribe! => Using set?
+      auto findDuplicateItr = std::find_if(subscribers.begin(), subscribers.end(), [subscriber](const Subscriber& subscriberInfo)
+      {
+         return (subscriberInfo.first == subscriber);
+      });
+
+      if (findDuplicateItr == subscribers.end())
+      {
+         subscribers.push_back(std::make_pair(
+            subscriber,
+            std::forward<Callback>(callback)));
+      }
+   }
+
+   void EventManager::UnSubscribe(void* unsubscriber, std::vector<Subscriber>& subscribers)
+   {
+      auto foundSubscriber = std::find_if(subscribers.begin(), subscribers.end(),
+         [&unsubscriber](const Subscriber& foundSubscriber) {
+         return foundSubscriber.first == unsubscriber;
+      });
+
+      if (foundSubscriber != subscribers.end())
+      {
+         subscribers.erase(foundSubscriber);
+      }
+   }
+
+   void EventManager::SubscribeImmeidate(uint32 eventType, void* subscriber, Callback&& callback)
+   {
+      m_events[eventType].push_back(std::make_pair(subscriber, std::forward<Callback>(callback)));
+   }
+   
    void EventManager::Notify(uint32 eventType, const EventDataType& data)
    {
       auto itr = m_events.find(eventType);
       if (itr != m_events.end())
       {
          std::vector<Subscriber>& subscribers = (*itr).second;
-         UnSubscribe(eventType, subscribers);
+         ProcessChangedState(eventType, subscribers);
          for (Subscriber& subscriber : subscribers)
          {
             subscriber.second(data);
--- Sources/Runtime/Core/EventManager.hpp
+++ Sources/Runtime/Core/EventManager.hpp
@@ -21,27 +21,38 @@ namespace rumia
       AppClose
    };
 
+   enum class ESubscriberState
+   {
+      Subscribe,
+      UnSubscribe
+   };
+
    using EventDataType = std::variant<bool, uint8, int8, uint16, int16, int16, uint32, int32, uint64, int64, float, double, std::string,
       Actor*, std::vector<Actor*>, glm::vec2, glm::vec3, glm::vec4, glm::mat4, glm::quat>;
    using Callback = std::function<void(EventDataType)>;
    using Subscriber =std::pair<void*, Callback>;
+   using StateChange = std::pair<ESubscriberState, Subscriber>;
 
    class RUMIA EventManager
    {
    public:
       static EventManager& GetInstance();
 
-      void Subscribe(uint32 eventType, void* subscriber, Callback&& callback);
-      void UnSubscribeEnqueue(uint32 eventType, void* subscriber);
+      void StageChangeEnqueue(uint32 eventType, ESubscriberState state, void* subscriber, Callback&& callback);
 
       void Notify(uint32 eventType, const EventDataType& data = 0);
 
    private:
-      void UnSubscribe(uint32 eventType, std::vector<Subscriber>& subscribers);
+      void ProcessChangedState(uint32 eventType, std::vector<Subscriber>& subscribers);
+      void Subscribe(void* subscriber, Callback&& callback, std::vector<Subscriber>& subscribers);
+      void UnSubscribe(void* unsubscriber, std::vector<Subscriber>& subscribers);
+
+      /* It must only use to initial subscribe! **/
+      void SubscribeImmeidate(uint32 eventType, void* subscriber, Callback&& callback);
 
    private:
       std::map<uint32, std::vector<Subscriber>> m_events;
-      std::map<uint32, std::vector<void*>> m_unsubQueueMap;
+      std::map<uint32, std::vector<StateChange>> m_stateChangeQueueMap;
 
    };
 }
@@ -49,8 +60,8 @@ namespace rumia
 #define EVENT_HANDLER(func) [this](EventDataType){func();}
 #define EVENT_HANDLER_DATA(func) [this](EventDataType data){func(data);}
 
-#define SUBSCRIBE_EVENT(eventType, subscriber, func) EventManager::GetInstance().Subscribe(eventType, subscriber, func);
-#define UNSUBSCRIBE_EVENT(eventType, subscriber) EventManager::GetInstance().UnSubscribeEnqueue(eventType, subscriber);
+#define SUBSCRIBE_EVENT(eventType, subscriber, func) EventManager::GetInstance().StageChangeEnqueue(eventType, ESubscriberState::Subscribe, subscriber, func);
+#define UNSUBSCRIBE_EVENT(eventType, subscriber) EventManager::GetInstance().StageChangeEnqueue(eventType, ESubscriberState::UnSubscribe, subscriber, [](EventDataType){});
 
 #define NOTIFY(eventType) EventManager::GetInstance().Notify(eventType);
 #define NOTIFY_WITH_DATA(eventType, data) EventManager::GetInstance().Notify(eventType, data);
