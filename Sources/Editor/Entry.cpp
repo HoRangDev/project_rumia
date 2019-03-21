18ed60adf3ed5f83e7ee88f762f79c73e281247c
Yang Kyowon <kyowonhorang@gmail.com>
2019-03-21 11:13:29 +0900

    Modified to use OpenGL  3.3

--- Sources/Editor/Entry.cpp
+++ Sources/Editor/Entry.cpp
@@ -56,9 +56,9 @@ int main()
 
    // @TODO: Impl editor!
    glfwInit();
-   const char* glsl_version = "#version 460";
-   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+   const char* glsl_version = "#version 330 core";
+   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
    GLFWwindow* window = glfwCreateWindow(1280, 720, "test", nullptr, nullptr);
