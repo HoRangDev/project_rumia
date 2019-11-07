0663eb306720a9182acb21a29ad6151c08f518a6
Yang Kyowon <kyowonhorang@gmail.com>
2019-11-07 15:08:50 +0900

    Delete all old codes for restart project

--- Sources/Runtime/Math/MathDefines.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-// Based on left-hand coordinate
-namespace rumia
-{
-   constexpr float PI = 3.1415927f;
-}
--- Sources/Runtime/Rendering/RenderingDefs.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#include <GL/gl3w.h>
-#include <glfw/glfw3.h>
